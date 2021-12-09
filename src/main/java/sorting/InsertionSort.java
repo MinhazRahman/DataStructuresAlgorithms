c324723648e099c28801f762413e6d644d09d406
Mohammed Rahman <minhaz357@gmail.com>
2021-12-09 18:25:24 -0500

    Write design idea and proof of correctness of Insertion sort

--- src/main/java/sorting/InsertionSort.java
+++ src/main/java/sorting/InsertionSort.java
@@ -6,10 +6,20 @@ package sorting;
 // post condition: The program returns the same array arr[0...n-1] in sorted array, where arr[0]<=arr[1]<= ... arr[n-2]<=arr[n-1].
 // If the input array is empty then the program returns an empty array.
 
+// *** Improve the Design Idea ***
+
 // Design Idea: Incremental approach
-// Having the sorted sub-array arr[0...j-1] and the unsorted sub-array arr[j...n-1] where j is an integer, 1 <= j <= n.
+// We divide the input array, arr[0...n-1] logically into two sub-arrays, arr[0...j-1] and arr[j...n-1] where j is an integer, 1 <= j <= n.
+// The sub-array arr[0...j-1] holds the sorted elements and the sub-array arr[j...n-1] consists of the unsorted elements.
 // We insert the element arr[j] at the proper place into the sorted sub-array, resulting in the sorted sub-array arr[0...j].
-// Incrementing j for the next iteration gives us the the sorted sub-array arr[0...j-1].
+// Incrementing j for the next iteration gives us the sub-array arr[0...j-1] that is already sorted.
+
+//Consider the input array is, arr = {5, 2, 3, 1 7}
+
+// Here, we will use two loops, the outer for loop will iterate over the array, arr[0...n-1] for j = 1 to j < arr.length.
+// The inner loop will move the element arr[j-1], arr[j-2], arr[j-3]... to the right in the array arr[0...j].
+// At the end of the inner loop we place the element arr[j] at proper position, resulting in the sorted sub-array arr[0..j].
+// We increment j at the start of next iteration, which gives the sorted sub-array arr[0...j-1].
 
 import java.util.Arrays;
 
@@ -45,4 +55,22 @@ public class InsertionSort {
 }
 
 // correctness proof of the Insertion sort algorithm.
-// Invariant: sub-array arr[0...j-1] always contains the sorted elements.
+// Guess Invariant: sub-array arr[0...j-1] always contains the sorted elements.
+
+// Tests of the Guess loop invariant.
+// Weak enough? Is the gI true on start or on initialization?
+// On initialization j = 1, the sorted sub-array arr[0] holds only one element. So, on start, the sub-array arr[0...j-1]
+// is trivially sorted. Yes, the guess invariant is weak enough.
+
+// Preserved? Is the gI true on each iteration?
+// Having the sorted sub-array arr[0...j-1], we insert arr[j] at the proper position in the sorted sub-array, resulting
+// in sorted sub-array arr[0...j]
+
+// Incrementing j for the next iteration of for loop then preserves the guess loop invariant.
+
+// Strong enough? Is the gI true on termination.
+// The loop controlling variable j is an integer and we increment j by 1 on each iteration. The for loop terminates
+// when j = n, where n is the length of the array.
+// Substituting n for j in the loop invariant, we have the sub-array arr[0...n-1] holds the elements originally in
+// arr[0...n-1] but in sorted order. The sub-array arr[0...n-1] is the entire array.
+// We conclude that the entire array is sorted. Hence, the algorithm is correct.

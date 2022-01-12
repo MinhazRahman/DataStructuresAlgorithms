0b2216b02ebd01de8d4fbaab6c6d520d17517301
Mohammed Rahman <minhaz357@gmail.com>
2022-01-12 12:39:31 -0500

    Fibonacci: iterative function

--- src/main/java/recursion/Fibonacci.java
+++ src/main/java/recursion/Fibonacci.java
@@ -1,6 +1,6 @@
 package recursion;
 
-// Fibonacci series: 1 1 2 3 5 8 13 21 34 55....
+// Fibonacci series: 0 1 1 2 3 5 8 13 21 34 55....
 // fib(n) = 0, if n=0
 // fib(n) = 1, if n=1
 // fib(n) = fib(n-1) + fib(n-2), if n>1
@@ -14,7 +14,39 @@ public class Fibonacci {
         // recursive step
         return fib(n-1) + fib(n-2);
     }
+
+    public static int fibonacci(int n){
+        int a = 0, b = 1, c = 0;
+        if (n == 0 || n == 1){
+            return n;
+        }
+
+        for (int i = 2; i <= n; i++){
+            c = a + b;
+            a = b;
+            b = c;
+        }
+
+        return c;
+    }
+
+    public static int fibIterative(int n){
+        // The array fib[0...n] holds the values of the previously completed computations
+        int[] fib = new int[n+1];
+        fib[0] = 0;
+        fib[1] = 1;
+
+        if (n == 0 || n == 1){
+            return n;
+        }
+
+        for (int i = 2; i<=n; i++){
+            fib[i] = fib[i-1] + fib[i-2];
+        }
+        return fib[n];
+    }
     public static void main(String[] args) {
+        System.out.println("Recursive fibonacci:");
         System.out.println(fib(0));
         System.out.println(fib(1));
         System.out.println(fib(2));
@@ -22,5 +54,9 @@ public class Fibonacci {
         System.out.println(fib(4));
         System.out.println(fib(5));
         System.out.println(fib(6));
+
+        System.out.println("Iterative fibonacci:");
+        System.out.println(fibonacci(6));
+        System.out.println(fibIterative(6));
     }
 }

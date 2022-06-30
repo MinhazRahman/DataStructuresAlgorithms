c8265e0276437d3f229595f841e772097dc4d55c
Mohammed Rahman <minhaz357@gmail.com>
2022-06-30 04:11:46 -0400

    Depth first search for graph

---
+++ src/main/java/graphs/GraphDFS.java
@@ -0,0 +1,116 @@
+package graphs;
+
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.Queue;
+
+/**
+ *  1 --------- 2
+ *  .  \        .
+ *  .       \   .
+ *  4 --------- 3
+ *     \      /
+ *      5
+ *     /  \
+ *  6       7
+ *
+ *  Understand:
+ *  - print all the visited vertices
+ *  - pre-condition:
+ *  - input: 2d array that represents the graph, int input
+ *  Match:
+ *  - DEPTH first search
+ *  Plan:
+ *  - start from a vertex, we will call it visited
+ *  - then explore the vertex
+ *  -
+ * */
+
+public class GraphDFS {
+
+    public static void graphDFS(int[][] edges, int startingVertex){
+        // We need a queue to store visited vertices, that we will explore later
+        Queue<Integer> queue = new LinkedList<>();
+        // We need an array to keep track of visited vertices
+        int[] visited = new int[edges.length];
+        Arrays.fill(visited, 0);
+
+        // mark the vertex as visited
+        visited[startingVertex] = 1;
+        // add the starting vertex to the queue
+        queue.add(startingVertex);
+
+        // as long as the queue is not empty, pop one vertex from the queue and explore it
+        while (!queue.isEmpty()){
+            // number of columns of the 2D array/adjacency matrix
+            int n = edges[0].length;
+            // pop the head element/vertex from the queue for exploration
+            int u = queue.poll();
+            System.out.print(u + " ");
+
+            // explore the vertex u
+            for (int v = 1; v<n; v++){
+                // if there is an edge and the vertex at the other end of the edge is not
+                // visited yet, then visit the vertex and add the visited vertex to the
+                // queue, so that we can explore it later
+                if (edges[u][v] == 1 && visited[v] == 0){
+                    // mark the vertex as visited
+                    visited[v] = 1;
+                    // add the vertex to the queue
+                    queue.add(v);
+                }
+            }
+        }
+    }
+
+    public static void dfs(int[][] edges, int n, int u, int[] visited){
+        if (visited[u] == 0){
+            System.out.print(u + " ");
+            visited[u] = 1;
+            // explore vertex u
+            for (int v = 1; v<n; v++){
+                // if there is an edge and the vertex at the other end of the edge is not
+                // visited yet, then visit the vertex recursively
+                if (edges[u][v] == 1 && visited[v] == 0){
+                    dfs(edges, n, v, visited);
+                }
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        // 8x8 graph, we will leave the 0th row and the 0th column empty
+        //               0  1  2  3  4  5  6  7
+        int[][] edges ={{0, 0, 0, 0, 0, 0, 0, 0}, // 0
+                        {0, 0, 1, 1, 1, 0, 0, 0}, // 1
+                        {0, 1, 0, 1, 0, 0, 0, 0}, // 2
+                        {0, 1, 1, 0, 1, 1, 0, 0}, // 3
+                        {0, 1, 0, 1, 0, 1, 0, 0}, // 4
+                        {0, 0, 0, 1, 1, 0, 1, 1}, // 5
+                        {0, 0, 0, 0, 0, 1, 0, 0}, // 6
+                        {0, 0, 0, 0, 0, 1, 0, 0}}; // 7
+
+        // number of columns of the 2D array/adjacency matrix
+        int n = edges[0].length;
+        // We need an array to keep track of visited vertices
+        int[] visited = new int[edges.length];
+        Arrays.fill(visited, 0);
+
+        dfs(edges, n, 4, visited);
+/**
+        graphDFS(edges, 1);
+        System.out.println();
+        graphDFS(edges, 2);
+        System.out.println();
+        graphDFS(edges, 3);
+        System.out.println();
+        graphDFS(edges, 4);
+        System.out.println();
+        graphDFS(edges, 5);
+        System.out.println();
+        graphDFS(edges, 6);
+        System.out.println();
+        graphDFS(edges, 7);
+ */
+    }
+}

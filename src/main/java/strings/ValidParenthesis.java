617476cef716a6e259d1ed0b31b1afc359192772
Mohammed Rahman <minhaz357@gmail.com>
2022-07-26 21:34:19 -0400

    Balanced parentheses

--- src/main/java/strings/ValidParenthesis.java
+++ src/main/java/strings/ValidParenthesis.java
@@ -54,13 +54,36 @@ public class ValidParenthesis {
         return stack.isEmpty();
     }
 
+    public static boolean isBalanced(String s){
+        int count = 0;
+
+        for (int i=0; i<s.length(); i++){
+            char ch = s.charAt(i);
+
+            if (count < 0) return false;
+
+            if (ch == '(') count++;
+
+            if (ch == ')') count--;
+        }
+
+        return count == 0;
+    }
+
     public static void main(String[] args) {
 
         System.out.println(isValidParenthesis("(()"));
         System.out.println(isValidParenthesis("("));
         System.out.println(isValidParenthesis(")"));
-        System.out.println(isValidParenthesis("(())"));
         System.out.println(isValidParenthesis("(()))"));
+        System.out.println(isValidParenthesis("(())"));
+
+        System.out.println();
+        System.out.println(isBalanced("(()"));
+        System.out.println(isBalanced("("));
+        System.out.println(isBalanced(")"));
+        System.out.println(isBalanced("(()))"));
+        System.out.println(isBalanced("(())"));
 
     }
 }

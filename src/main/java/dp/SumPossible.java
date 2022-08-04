49b9b8fc1aa6bcca1e2f3e2bb908b1818887005e
Mohammed Rahman <minhaz357@gmail.com>
2022-08-04 15:26:37 -0400

    DP: sum possible

--- src/main/java/dp/SumPossible.java
+++ src/main/java/dp/SumPossible.java
@@ -3,21 +3,28 @@ package dp;
 public class SumPossible {
 
     public static boolean isSumPossible(int amount, int[] numbers){
+        if (amount < 0){
+            return  false;
+        }
         if (amount == 0){
             return true;
         }
 
         for (int number : numbers) {
-            if (number <= amount) {
-                return isSumPossible(amount - number, numbers);
+            if (isSumPossible(amount - number, numbers)) {
+                return true;
             }
         }
         return false;
     }
 
     public static void main(String[] args) {
-        int[] arr = {6, 2, 10, 19};
-        int amount = 15;
+        int[] arr = {1, 2, 3};
+        int amount = 5;
+        System.out.println(isSumPossible(amount, arr));
+
+        arr = new int[]{6, 2, 10, 19};
+        amount = 15;
         System.out.println(isSumPossible(amount, arr));
     }
 }

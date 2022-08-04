0a73e94b0b62e1bdae6ef58e56c6fa16ef686f12
Mohammed Rahman <minhaz357@gmail.com>
2022-08-04 18:24:42 -0400

    DP: sum possible using memoization

--- src/main/java/dp/SumPossible.java
+++ src/main/java/dp/SumPossible.java
@@ -2,6 +2,7 @@ package dp;
 
 public class SumPossible {
 
+    // using recursion
     public static boolean isSumPossible(int amount, int[] numbers){
         if (amount < 0){
             return  false;
@@ -18,6 +19,24 @@ public class SumPossible {
         return false;
     }
 
+    // using memoization and recursion
+    public static boolean isSumPossible(int amount, int[] numbers, boolean[] memo){
+        if (memo[amount]) return memo[amount];
+
+        if (amount == 0) return true;
+
+        for (int number : numbers) {
+            if (number <= amount){
+                if (isSumPossible(amount - number, numbers, memo)) {
+                    memo[amount] = true;
+                    return true;
+                }
+            }
+        }
+        memo[amount] = false;
+        return false;
+    }
+
     public static void main(String[] args) {
         int[] arr = {1, 2, 3};
         int amount = 5;
@@ -26,5 +45,9 @@ public class SumPossible {
         arr = new int[]{6, 2, 10, 19};
         amount = 15;
         System.out.println(isSumPossible(amount, arr));
+
+        // using memo and recursion
+        boolean[] memo = new boolean[amount + 1];
+        System.out.println(isSumPossible(amount, arr, memo));
     }
 }

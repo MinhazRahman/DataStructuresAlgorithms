31436a7fef5b7457e07b5da099e6d6c04cf7520a
Mohammed Rahman <minhaz357@gmail.com>
2022-08-10 01:04:28 -0400

    Find the shortest subarray length

--- src/main/java/slidingWindow/SmallestSubArraySum.java
+++ src/main/java/slidingWindow/SmallestSubArraySum.java
@@ -4,7 +4,9 @@ import java.util.Arrays;
 
 /**
  Problem Statement#
- Given an array of positive integers and a number ‘S,’ find the length of the smallest contiguous subarray whose sum is greater than or equal to ‘S’. Return 0 if no such subarray exists.
+ Given an array of positive integers and a number ‘S,’
+ find the length of the smallest contiguous subarray whose sum is greater than or equal to ‘S’.
+ Return 0 if no such subarray exists.
 
  Example 1:
 
@@ -49,12 +51,35 @@ public class SmallestSubArraySum {
        return  sum;
     }
 
+    public static int shortestSubArray(int[] nums, int k) {
+        int n = nums.length;
+        int smallestLength = Integer.MAX_VALUE;
+        int start = 0;
+        int sum = 0;
+        int oldSum = 0;
+        for(int end=0; end<n; end++){
+            // Add the next element to the window
+            sum += nums[end];
+            // Shrink the window as small as possible until the 'windowSum' is smaller than 'K'
+            while(sum >= k){
+                smallestLength = Math.min(smallestLength, end-start+1);
+                // Discard the element at 'windowStart' since it is going out of the window
+                sum -= nums[start];
+                // Shrink the window
+                start++;
+            }
+        }
+
+        return smallestLength == Integer.MAX_VALUE? -0: smallestLength;
+    }
+
     public static void main(String[] args) {
         int[] input = new int[]{2, 1, 5, 2, 3, 2};
 
-        System.out.println(findSmallestSubArraySum(input, 7));
+        System.out.println(shortestSubArray(input, 7));
+
 
         input = new int[] {3, 4, 1, 1, 6};
-        System.out.println(findSmallestSubArraySum(input, 8));
+        System.out.println(shortestSubArray(input, 8));
     }
 }

4dcb1364d725810b3cbdd6a201508c44476b35fe
Mohammed Rahman <minhaz357@gmail.com>
2022-10-03 12:00:34 -0400

    Undirected graph path DFS

--- src/main/java/graph/algos/UndirectedPath.java
+++ src/main/java/graph/algos/UndirectedPath.java
@@ -61,7 +61,7 @@ import java.util.*;
 public class UndirectedPath {
 
     // The following method uses BFS to check if there is path between the 'src' and 'dst' nodes
-    public static boolean undirectedPath(char[][] edges, Character src, Character dst){
+    public static boolean undirectedPathBFS(char[][] edges, Character src, Character dst){
         Map<Character, List<Character>> graph = buildGraph(edges);
         Queue<Character> queue = new LinkedList<>();
         Set<Character> visited = new LinkedHashSet<>();
@@ -86,6 +86,26 @@ public class UndirectedPath {
      * Time complexity: O(e)
      * Space complexity: O(n)
      * */
+
+    // The following method uses DFS to check if there is path between the 'src' and 'dst' nodes
+    public static boolean undirectedPathDFS(char[][] edges, Character src, Character dst){
+        Map<Character, List<Character>> graph = buildGraph(edges);
+        Stack<Character> stack = new Stack<>();
+        Set<Character> visited = new LinkedHashSet<>();
+
+        if (src != null) stack.push(src);
+
+        while (!stack.isEmpty()){
+            Character current = stack.pop();
+            visited.add(current);
+            if (current == dst) return true;
+            List<Character> neighbors = graph.get(current);
+            for (Character neighbor: neighbors){
+                if (!visited.contains(neighbor)) stack.push(neighbor);
+            }
+        }
+        return false;
+    }
 
     public static Map<Character, List<Character>> buildGraph(char[][] edges){
         // an unordered undirected map that represents a graph
@@ -128,17 +148,31 @@ public class UndirectedPath {
             System.out.println(entry.getKey() +": " + entry.getValue());
         }
 
-        System.out.println(undirectedPath(edges, 'j', 'm'));
+        System.out.println("Undirected path BFS: ");
+        System.out.println(undirectedPathBFS(edges, 'j', 'm'));
 
-        System.out.println(undirectedPath(edges, 'm', 'j'));
+        System.out.println(undirectedPathBFS(edges, 'm', 'j'));
 
-        System.out.println(undirectedPath(edges, 'l', 'j'));
+        System.out.println(undirectedPathBFS(edges, 'l', 'j'));
 
-        System.out.println(undirectedPath(edges, 'k', 'o'));
+        System.out.println(undirectedPathBFS(edges, 'k', 'o'));
 
-        System.out.println(undirectedPath(edges, 'i', 'o'));
+        System.out.println(undirectedPathBFS(edges, 'i', 'o'));
 
-        System.out.println(undirectedPath(edges, 'o', 'n'));
+        System.out.println(undirectedPathBFS(edges, 'o', 'n'));
+
+        System.out.println("Undirected path DFS: ");
+        System.out.println(undirectedPathDFS(edges, 'j', 'm'));
+
+        System.out.println(undirectedPathDFS(edges, 'm', 'j'));
+
+        System.out.println(undirectedPathDFS(edges, 'l', 'j'));
+
+        System.out.println(undirectedPathDFS(edges, 'k', 'o'));
+
+        System.out.println(undirectedPathDFS(edges, 'i', 'o'));
+
+        System.out.println(undirectedPathDFS(edges, 'o', 'n'));
 
     }
 }

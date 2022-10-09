c8c7882f8817149e9800384d2380ea72d7aa75a9
Mohammed Rahman <minhaz357@gmail.com>
2022-10-09 12:15:29 -0400

    Binary search

--- src/main/java/searching/BinarySearch.java
+++ src/main/java/searching/BinarySearch.java
@@ -11,17 +11,17 @@ package searching;
 
 public class BinarySearch {
 
-    public static int binarySearch(int[] arr, int val){
+    public static int binarySearch(int[] arr, int target){
         // declare two pointers
         int left = 0, right = arr.length - 1;
 
         while (left <= right){
             // Calculate the mid
             int mid = (left + right)/2;
-            if (val == arr[mid]){
+            if (target == arr[mid]){
                 return mid;
             }
-            else if (val < arr[mid]){
+            else if (target < arr[mid]){
                 right = mid - 1;
             }
             else { // val > arr[mid]
@@ -32,6 +32,32 @@ public class BinarySearch {
         return -1;
     }
 
+    public static int search(int[] arr, int target){
+        // declare two pointers
+        int left = 0, right = arr.length - 1;
+
+        while (left <= right){
+            // Calculate the mid
+            int mid = (left + right)/2;
+
+            if (arr[mid] > target){ // look into the left half of the array
+                right = mid - 1;
+            }
+            else if(arr[mid] < target){ // look into the right half of the array
+                left = mid + 1;
+            }else{ // we have found the target value
+                return mid;
+            }
+        }
+        // not found
+        return -1;
+    }
+    /** Complexity analysis
+     * n = number of elements in the array
+     * time complexity: O(log n)
+     * space complexity: O(1), we are not using any extra space to store the processed data
+     * */
+
     public static void main(String[] args) {
         int[] ints = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
 
@@ -39,6 +65,12 @@ public class BinarySearch {
         System.out.println(binarySearch(ints, 15));
         System.out.println(binarySearch(ints, 10));
         System.out.println(binarySearch(ints, 7));
+
+        System.out.println();
+        System.out.println(search(ints, 5));
+        System.out.println(search(ints, 15));
+        System.out.println(search(ints, 10));
+        System.out.println(search(ints, 7));
 
     }
 }

0900f61829b193965498956f475dd7d4cc55e38c
Mohammed Rahman <minhaz357@gmail.com>
2023-01-07 08:03:32 +0600

    Fix implementation of enqeue
    	- enqeue(), rear: insert? Yes, delete? No
    	- dequeue(), front: insert? No, delete? Yes

--- src/main/java/queue/QueueLinkedList.java
+++ src/main/java/queue/QueueLinkedList.java
@@ -8,18 +8,23 @@ public class QueueLinkedList {
 
     }
 
+    // insert at the back of the queue
+    // use rear to insert values
     public void enqueue(int val){
         Node node = new Node(val);
 
+        // heap memory if full, we can't create any object in the heap
+        if (isFull()) return;
+
         if (isEmpty()){
             front = node;
-            rear = node;
         }else {
-            node.next = front;
-            front = node;
+            rear.next = node;
         }
+        rear = node;
     }
 
+    // use front to delete values
     public int dequeue(){
         int val = -1;
         if (!isEmpty()){

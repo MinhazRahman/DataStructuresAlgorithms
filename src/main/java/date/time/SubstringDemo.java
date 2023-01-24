815c13d45f5b85c2e13c1f75626b802e84e0f9ce
Mohammed Rahman <minhaz357@gmail.com>
2023-01-24 23:06:49 +0600

    Extract a string

---
+++ src/main/java/date/time/SubstringDemo.java
@@ -0,0 +1,17 @@
+package date.time;
+
+public class SubstringDemo {
+    public static void main(String[] args) {
+        String str = "P - PASSPORT";
+        System.out.println(str.substring(1));
+        System.out.println(str.substring(2));
+        System.out.println(str.substring(3));
+
+        String subStr = str.substring(3).trim();
+        System.out.println(subStr);
+        String first = subStr.substring(0,1);
+        System.out.println(first);
+        String capDocType = subStr.charAt(0) + subStr.substring(1).toLowerCase();
+        System.out.println(capDocType);
+    }
+}

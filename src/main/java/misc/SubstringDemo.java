7d1d21cf6da78bb13ef2f4350dbbb3dc33dacce8
Mohammed Rahman <minhaz357@gmail.com>
2023-02-02 21:00:37 +0600

    Replace parentheses from the string

--- src/main/java/misc/SubstringDemo.java
+++ src/main/java/misc/SubstringDemo.java
@@ -1,13 +1,17 @@
 package misc;
 
 public class SubstringDemo {
-    public static void main(String[] args) {
-        String str = "(123) 456-3920";
+    public static String replaceParenthesis(String str){
         // remove parentheses from the string and replace ")" with "-"
         String result = str.replaceAll("\\(", "").
-                            replaceAll("\\)", "-").
-                            replaceAll("\\s+", "");
+                replaceAll("\\)", "-").
+                replaceAll("\\s+", "");
+        return result;
+    }
+    public static void main(String[] args) {
 
-        System.out.println(result);
+        System.out.println(replaceParenthesis("123-456-3920"));
+        System.out.println(replaceParenthesis("123.456.3920"));
+        System.out.println(replaceParenthesis("(123) 456-3920"));
     }
 }

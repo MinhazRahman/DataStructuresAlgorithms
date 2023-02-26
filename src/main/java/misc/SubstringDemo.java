391413c7b117a5d9b8eecd782632f2a8b6e9bec4
Mohammed Rahman <minhaz357@gmail.com>
2023-02-26 13:33:54 -0500

    Replace matched string

--- src/main/java/misc/SubstringDemo.java
+++ src/main/java/misc/SubstringDemo.java
@@ -13,5 +13,14 @@ public class SubstringDemo {
         System.out.println(replaceParenthesis("123-456-3920"));
         System.out.println(replaceParenthesis("123.456.3920"));
         System.out.println(replaceParenthesis("(123) 456-3920"));
+
+        // if there is no matching then replaceAll gives error or
+        // simply returns the string
+        String str = "This is a string";
+        String replacedString1 = str.replaceAll("This", "It");
+        System.out.println(replacedString1);
+        String replacedString2 = str.replaceAll("That", "It"); // returns the same string
+        System.out.println(replacedString2);
+
     }
 }

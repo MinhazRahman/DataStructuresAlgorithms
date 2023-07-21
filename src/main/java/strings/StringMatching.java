966ccb81ea4055551329f6acd32339265f760505
Mohammed Rahman <minhaz357@gmail.com>
2023-07-20 22:47:11 -0400

    String matching

---
+++ src/main/java/strings/StringMatching.java
@@ -0,0 +1,11 @@
+package strings;
+
+public class StringMatching {
+    public static void main(String[] args) {
+        String s1 = "Mohammed Rahman";
+        String s2 = "Nothing";
+
+       boolean b = s1.contains(s2);
+        System.out.println(b);
+    }
+}
